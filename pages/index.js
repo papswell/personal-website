f973f65b615a8e7645732fc2eac1a582a8d3685d
Pierre Guillaume <papswell@gmail.com>
2020-04-17 19:39:16 +0200

    maintenance mode :P

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import CenteredLayout from "../components/layouts/CenteredLayout";
 export default function Index() {
   return (
     <CenteredLayout>
-      <p>Hello Next.js</p>
+      <p>Hi ! This website is not ready yet, come back later :)</p>
 
-      <Link href="/about">
+      {/* <Link href="/about">
         <a title="About Page">About Page</a>
-      </Link>
+      </Link> */}
     </CenteredLayout>
   );
 }
