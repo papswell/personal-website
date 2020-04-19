d17aa9f4133799944da5b70367a63ee5f762c0f8
Pierre Guillaume <papswell@gmail.com>
2020-04-19 17:22:01 +0200

    using typit lib for typewrittter effect

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,76 @@
 import Link from "next/link";
 import FullScreen from "../components/layouts/FullScreen";
 import Footer from "../components/footer";
-import Typewritter from "../components/typewritter";
+
+import TypeIt from "typeit-react";
+import { useState } from "react";
 
 export default function Index() {
+  const [run, makeRun] = useState(true);
   return (
     <FullScreen className="page">
       <main>
         <div className="content">
-          <h1>
-            <Typewritter speed={100}>Hi, I am Pierre.</Typewritter>
-          </h1>
+          {run ? (
+            <TypeIt
+              element={"div"}
+              getBeforeInit={(instance) => {
+                instance
+                  .type('<span class="h1">Hi, I\'m Pierre.</span><br><br>')
+                  .pause(750)
+                  .type("I can ")
+                  .type("<strong>code</strong>")
+                  .type(" a project <em>for</em> you…")
+                  .pause(1050)
+                  .move(-19)
+                  .delete("4")
+                  .type("<strong>design</strong>")
+                  .pause(900)
+                  .delete(6)
+                  .type("<strong>test</strong>")
+                  .pause(900)
+                  .delete("4")
+                  .type("<strong>deploy</strong>")
+                  .pause(800)
+                  .delete("6")
+                  .type("<strong>audit</strong>")
+                  .pause(500)
+                  .delete("5")
+                  .type("<strong>manage</strong>")
+                  .pause(400)
+                  .delete("6")
+                  .type(
+                    "<strong>audit</strong>, <strong>design</strong>, <strong>code</strong>, <strong>test</strong>, <strong>deploy</strong>, and <strong>manage</strong>",
+                    { speed: 70 }
+                  )
+                  .move("end", { speed: 20 })
+                  .pause(2200)
+                  .type(
+                    "<br><br>But I <strong>develop</strong> <em>with</em> you"
+                  )
+                  .pause(1050)
+                  .type(" <strong>:)</strong>");
 
-          <p>
-            I can{" "}
-            <Typewritter speed={100} delay={3000} className="strong">
-              audit, design, code, test, deploy, and manage
-            </Typewritter>
-            <br />a project <em>for you&nbsp;…</em>
-          </p>
+                return instance;
+              }}
+            ></TypeIt>
+          ) : (
+            <>
+              <h1>Hi, I'm Pierre.</h1>
 
-          <p>
-            But I{" "}
-            <Typewritter speed={100} delay={6000} className="strong">
-              develop
-            </Typewritter>
-            <em>with you</em> :)
-          </p>
+              <p>
+                I can <strong>audit</strong>, <strong>design</strong>,{" "}
+                <strong>code</strong>, <strong>test</strong>,{" "}
+                <strong>deploy</strong>, and <strong>manage</strong> a projet{" "}
+                <em>for you…</em>
+              </p>
+
+              <p>
+                But i <strong>develop</strong> <em>with</em> you{" "}
+                <strong>:)</strong>
+              </p>
+            </>
+          )}
         </div>
       </main>
 
